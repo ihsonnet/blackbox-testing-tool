94189a38098f80655cb07940bc80a76c1c4c77f9
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-06-28 05:17:15 +0600

    dynamic field + result prototype

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,12 +12,12 @@
           <a
             href="https://community.vuetifyjs.com"
             target="_blank"
-          >Discord Community</a>
+          >Facebook Community</a>
         </p>
       </v-col>
     </v-row>
     <v-row>
-      <v-card class="mx-auto pa-3" style="border-radius:15px;border: 1px solid #e7e7e7" elevation="0" width="820px">
+      <v-card class="mx-auto pa-5" style="border-radius:15px;border: 1px solid #e7e7e7" elevation="0" width="820px">
         <v-card-title>Add Requirment</v-card-title>
         <v-form
           class="ma-3"
@@ -25,86 +25,135 @@
           v-model="valid"
           lazy-validation
         >
-        <v-row>
+
+<!-- dynamic row -->
+        <v-row class="text-field" v-for="input,idx in requirements" :key="idx">
           <v-col>
             <v-select
-            v-model="select"
+            v-model="input.key"
             :items="items"
             :rules="[v => !!v || 'Item is required']"
             label="Key"
             required
             outlined
+            dense
           ></v-select>
           </v-col>
           <v-col>
             <v-text-field
-            v-model="name"
+            v-model="input.value"
             :counter="10"
             :rules="nameRules"
             label="Value"
+            :placeholder="input.key=='Input Type' ? 'ex. Text , Email , Password etc.' : 'ex. 0-30'"
             required
             outlined
+            dense
           ></v-text-field>
           </v-col>
+          <v-col cols="2">
+            <v-btn :disabled="requirements.length<='1'" @click="deleteReq(input,requirements)" justify="center" rounded elevation="0" class="mx-auto" color="error lighten-1">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-col>
         </v-row>
+
+<!-- add btn  -->
         <v-row>
           <v-col>
             <v-btn
-              color="warning"
-              @click="resetValidation"
+              color="warning lighten-1"
+              @click="addReq(1,requirements)"
+              elevation="0"
             >
               Add New
             </v-btn>
           </v-col>
         </v-row>
+
+<!-- selector -->
         <v-row class="pb-5">
           <v-col>
             <v-checkbox
-            v-model="checkbox1"
-            :rules="[v => !!v || 'You must agree to continue!']"
+            v-model="issue"
+            :rules="[issue==true || ecp==true || vba==true || 'You must Select at least one calculate method.']"
             label="Testing Issue"
-            required
           ></v-checkbox>
           </v-col>
           <v-col>
             <v-checkbox
-            v-model="checkbox2"
-            :rules="[v => !!v || 'You must agree to continue!']"
+            v-model="ecp"
+            :rules="[issue==true || ecp==true || vba==true || 'You must Select at least one calculate method.']"
             label="Equvalance Pertisioning"
-            required
           ></v-checkbox>
           </v-col>
           <v-col>
             <v-checkbox
-            v-model="checkbox3"
-            :rules="[v => !!v || 'You must agree to continue!']"
+            v-model="vba"
+            :rules="[issue==true || ecp==true || vba==true || 'You must Select at least one calculate method.']"
             label="Boundary Value Analysis"
-            required
           ></v-checkbox>
           </v-col>
         </v-row>
           <v-btn
-            color="error"
+            color="error lighten-1"
             class="mr-4"
             @click="reset"
+            elevation="0"
           >
             Reset Form
           </v-btn>
           <v-btn
             :disabled="!valid"
-            color="success"
+            color="success lighten-1"
             class="mr-4"
             @click="validate"
+            elevation="0"
           >
             Calculate
           </v-btn>
         </v-form>
       </v-card>
     </v-row>
-    <v-card width="100%" class="mt-10" flat style="background-color:#e7e7e7;">
+    <v-card width="100%" class="mt-10 pa-3" flat style="background-color:#e7e7e7;border-radius:15px">
       <v-card-title align="center" class="mx-auto">
         Testing Result
       </v-card-title>
+      <v-divider color="#e7e7e7"></v-divider>
+       <v-card-text v-if="calculate">
+         <v-row v-if="issue">
+         <v-col>
+            <h3>Testing Issues</h3>
+          * Comming Soon
+         </v-col>
+        </v-row>
+        <br>
+        <v-row v-if="ecp">
+         <v-col>
+            <h3>Equbalance Class Pertisioning</h3>
+          * Comming Soon {{calculate}}
+         </v-col>
+        </v-row>
+        <br>
+         <v-row v-if="vba">
+         <v-col>
+            <h3>Boundary Value Analysis</h3>
+            <br>
+            <v-row class="mx-auto" style="width:800px;text-align:center;border-bottom: 2px solid gray">
+              <v-col>
+                <div style="text-align:right">-1<span style="margin-left:30px;color:red" ><b>0</b></span></div>
+              </v-col>
+              <v-col>
+                <div style="margin-left:20px;text-align:left">1<span style="text-align:right;margin-left:190px;" >29</span></div>
+              </v-col>
+              <v-col>
+                <div style="text-align:left"><span style="margin-right:30px;color:red" ><b>30</b></span>31</div>
+              </v-col>
+            </v-row>
+         </v-col>
+        </v-row>
+       </v-card-text>
+       <br>
     </v-card>
   </v-container>
 </template>
@@ -114,11 +163,22 @@
     name: 'HelloWorld',
 
     data: () => ({
+      idx:0,
+      issue: false,
+      ecp: false,
+      vba: false,
+      calculate: false,
+      requirements: [
+        {
+          key: "",
+          value: ""
+        }
+      ],
       valid: true,
       name: '',
       nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        v => !!v || 'Value is required',
+        v => (v && v.length <= 10) || 'Value must be less than 10 characters',
       ],
       email: '',
       emailRules: [
@@ -127,8 +187,8 @@
       ],
       select: null,
       items: [
-        'Data Type',
-        'Length',
+        'Input Type',
+        'Range',
       ],
       checkbox1: false,
       checkbox2: false,
@@ -137,11 +197,25 @@
     }),
 
     methods: {
+      addReq(input,field){
+        field.push({key:"",value: ""});
+        console.log(this.requirements)
+      },
+      deleteReq(index,field){
+        field.splice(index, 1)
+      },
       validate () {
         this.$refs.form.validate()
+        if(this.$refs.form.validate()){
+          this.calculate=true;
+        }
+        else{
+          this.calculate=false;
+        }
       },
       reset () {
         this.$refs.form.reset()
+        this.calculate=false;
       },
       resetValidation () {
         this.$refs.form.resetValidation()
@@ -149,3 +223,9 @@
     },
   }
 </script>
+<style>
+.text-field .col {
+  padding-bottom: 0 !important;
+  padding-top: 0 !important;
+}
+</style>
